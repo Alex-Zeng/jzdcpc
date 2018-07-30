6db00751b920eac68e7f74524b9823e33d3be735
wuxinqian <wuxinqian@jizhongdiancai.com>
2018-07-30 13:58:53 +0800

    集众服务

---
+++ static/service/js/javascript.js
@@ -0,0 +1,49 @@
+$(function(){
+    $('.product ul li img').click(function () {
+        if ($(this).attr('src') == './img/product-btn-1.png') {
+            $(this).attr('src','./img/product-btn-2.png')
+        } else {
+            $(this).attr('src','./img/product-btn-1.png')
+        }
+        $(this).parent().next().toggleClass('product-info-hide')
+    })
+    $('.main-form button[type="button"]').click(function () {
+        $('.main-form button[type="button"]').removeClass('active')
+        $(this).addClass('active')
+    })
+    $('.carousel-btn a:first-child').click(function () {
+        $('.carousel').animate({'left': '0px'})
+        return false
+    })
+    $('.carousel-btn a:last-child').click(function () {
+        $('.carousel').animate({'left': '-1300px'})
+        return false
+    })
+    function submit(val) {
+        var name = $('#name').val()
+        var sex = $('#sex').find('.active').text()
+        var phone = $('#phone').val()
+        $.ajax({
+            type: 'post',
+            url: '/papi/service/finance',
+            data: {
+                'name': name,
+                'sex': sex,
+                'phone': phone,
+                'type': val
+            },
+            success (data) {
+                console.log('success')
+            },
+            error () {
+                console.log('error')
+            }
+        })
+    }
+    $('.submit').each(function () {
+        $(this).click(function () {
+            submit($(this).attr('id'))
+            return false
+        })
+    })
+})
