54839ea880418f897c8334292cd35c054db50aa2
wuxinqian <wuxinqian@jizhongdiancai.com>
2018-08-10 09:52:01 +0800

    8-10

--- middleware/lockGroup.js
+++ middleware/lockGroup.js
@@ -1,7 +1,7 @@
 export default function ({ store, redirect }) {
   if (store.getters.loggedUser) {
     const user = store.getters.loggedUser
-    if (user.group != 4) {
+    if (user.group == 5) {
       redirect('/')
     }
   }
---
+++ plugins/zoom.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import zoom from 'vue-piczoom'
+
+export default () => {
+  Vue.use(zoom)
+}
