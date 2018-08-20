b3242b163791babcccabbe7ddbcc82eb468997ac
杨旭 <yangxu@jizhongdiancai.com>
2018-08-20 15:06:56 +0800

    update bug

--- components/PicZoom.vue
+++ components/PicZoom.vue
@@ -142,7 +142,7 @@
                     imgsrc=this.bigImgUrl
                 }else{
                     imgsrc=this.imgUrl
-                    
+
                 }
                 this.img=new Image()
                 this.img.src=imgsrc
@@ -155,7 +155,7 @@
                         this.rectTimesY=(this.imgbox.offsetHeight/this.scale)/box.querySelector('img').offsetHeight
                     }, 20);
                 }
-                
+
             },
             mousemove(e){
                 if(!this.init){
@@ -175,13 +175,13 @@
                         totalLeft+=par.clientLeft;
                         totalTop+=par.clientTop
                         }
-                        
+
                         //累加父级参照物本身的偏移
                         totalLeft+=par.offsetLeft;
                         totalTop+=par.offsetTop
                         par = par.offsetParent;
                     }
-                
+
                     return{
                         left:totalLeft,
                         top:totalTop
@@ -191,7 +191,7 @@
                 function getXY(eve) {
                     return {
                         x : eve.clientX -(_this.cover.offsetWidth/2),
-                        y : eve.clientY-(_this.cover.offsetHeight/2) 
+                        y : eve.clientY-(_this.cover.offsetHeight/2)
                     };
                 }
                 let oEvent = e || event;
@@ -221,7 +221,7 @@
                 let startX=pos.x-(imgwrap.left-document.documentElement.scrollLeft),
                 startY=pos.y-(imgwrap.top-document.documentElement.scrollTop)
                 this.ctx.drawImage(this.img,startX*this.imgTimesX,startY*this.imgTimesY,this.img.width*this.rectTimesX,this.img.height*this.rectTimesY,0,0,this.imgbox.offsetWidth,this.imgbox.offsetHeight);
-                
+
             },
             mouseover(e){
                 if(!this.init){
@@ -237,7 +237,7 @@
                         ev.preventDefault();
                     },false);
                 }
-                
+
                 this.cover.style.display='block'
                 this.canvas.style.display='block'
             },
@@ -263,30 +263,30 @@
                         this.rotateImg(bigOrginImg,direction,this.bigStep,true)
                     }
                 }
-                
+
             },
             rotateImg(img,direction,step,isBig=false){
                 var min_step = 0;
                 var max_step = 3;
                 if (img == null) return;
-                //img的高度和宽度不能在img元素隐藏后获取，否则会出错    
+                //img的高度和宽度不能在img元素隐藏后获取，否则会出错
                 var height = img.height;
                 var width = img.width;
-                
+
                 if (step == null) {
                     step = min_step;
                 }
                 if (direction == 'right') {
                     step++;
-                    //旋转到原位置，即超过最大值    
+                    //旋转到原位置，即超过最大值
                     step > max_step && (step = min_step);
                 } else {
                     step--;
                     step < min_step && (step = max_step);
-                }   
-                var canvas = document.createElement('canvas')  
-                
-                //旋转角度以弧度值为参数    
+                }
+                var canvas = document.createElement('canvas')
+
+                //旋转角度以弧度值为参数
                 var degree = step * 90 * Math.PI / 180;
                 var ctx = canvas.getContext('2d');
                 canvas.width = height;
@@ -319,7 +319,7 @@
                         break;
                 }
                 var newImg=canvas.toDataURL()
-                
+
                 if(isBig){
                     this.bigImgUrl=newImg
                     this.bigStep=step
@@ -356,12 +356,12 @@
         img{
             width: 100%;
             height: 100%;
-        };
+        }
         .mouse-cover{
             position: fixed;
             background-color: rgba(0,0,0,0.5);
-            cursor:move
-        };
+            cursor:move;
+        }
         .mouse-cover-canvas{
             position:fixed;
             left:100%;
