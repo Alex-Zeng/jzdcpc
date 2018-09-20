282ddba78a48b3173195744ef0a6f7ad9f9448b2
曾德辉 <zengdehui@jizhongdiancai.com>
2018-09-20 17:53:34 +0800

    生产环境配置修改

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,7 +90,8 @@ module.exports = {
   proxy: {
     '/papi': {
       // target: 'http://192.168.3.135:80/api',
-      target: 'http://jwapi-uat.jizhongdiancai.com/api',
+      // target: 'http://jwapi-uat.jizhongdiancai.com/api',
+      target: 'http://scm.jizhongdiancai.com/api',
       pathRewrite: { '^/papi': '' }
     }
   }
