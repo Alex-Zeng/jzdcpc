ac98dc1b48800209977dcb9bc09898f120c258a1
yangxu <yangxu@jizhongdiancai.com>
2018-10-09 10:49:01 +0800

    导航加图标

--- components/index/header.vue
+++ components/index/header.vue
@@ -35,7 +35,7 @@
           <li class="itemAll"><i class="menu-icon">&#xe605;</i>全部商品分类</li>
           <li :class="{item:true, isOpen: isOpen || show, active: (child===i.child) && showWrap}" v-for="(i ,k) in menu" :key="'child'+k + i.id" @mouseover="selectId = i.id, child = i.child, showWrap=true, slogan=false">
             <div class="img">
-              <img :src="i.web_path" width="16px" alt="">
+              <img :src="i.iconWeb" width="20" height="20" alt="">
             </div>
             <span style="float: left;">{{i.name}}</span>
           </li>
@@ -240,6 +240,9 @@ export default {
           margin-right 20px
           img
             vertical-align:middle
+            display inline-block
+            width 20px
+            height 20px
         &.isOpen
           display block
         &:after
