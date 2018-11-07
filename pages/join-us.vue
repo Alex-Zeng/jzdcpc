3bdb5c352d2fb554c19e96e06b093d25eb77afad
yangxu <yangxu@jizhongdiancai.com>
2018-11-07 16:02:27 +0800

    招商

--- pages/join-us.vue
+++ pages/join-us.vue
@@ -126,9 +126,7 @@ export default {
   data () {
     var validatePhone = (rule, value, callback) => {
       const trimVal = value.trim()
-      if (!trimVal) {
-        return callback(new Error('手机号不能为空'))
-      } else if (!phoneReg.test(trimVal)) {
+      if (!phoneReg.test(trimVal)) {
         return callback(new Error('手机号格式不正确'))
       } else {
         callback()
@@ -145,6 +143,7 @@ export default {
           {required: true, message: '请输入企业名称', trigger: 'blur'}
         ],
         contactNum: [
+          {required: true, message: '请输入手机号', trigger: 'blur'},
           {validator: validatePhone, trigger: 'blur'}
         ],
         contact: [
